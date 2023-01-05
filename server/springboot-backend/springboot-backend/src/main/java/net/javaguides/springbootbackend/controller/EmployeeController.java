bdb2b517d7720a3c96686e2401f1f3aee1d70a7d
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-01-04 23:32:46 -0300

    Spring + MySQL configuration

---
+++ server/springboot-backend/springboot-backend/src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package net.javaguides.springbootbackend.controller;
+
+import java.util.List;
+
+import net.javaguides.springbootbackend.model.Employee;
+import net.javaguides.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+
+}
