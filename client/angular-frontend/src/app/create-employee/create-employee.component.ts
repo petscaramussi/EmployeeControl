6e74ea5d95afb08d4dc5050779fc5c1a72be4311
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-01-17 18:00:40 -0300

    Spring add READ and UPDATE methods

--- client/angular-frontend/src/app/create-employee/create-employee.component.ts
+++ client/angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,13 +12,30 @@ export class CreateEmployeeComponent implements OnInit {
 
   employee: Employee = new Employee();
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
+  saveEmployee() {
+
+    this.employeeService.createEmployee(this.employee).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => {console.info('complete'); this.goToEmployeeList()}
+    });
+
+
+
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['/employees']);
+  }
+
+  onSubmit() {
     console.log(this.employee);
+    this.saveEmployee();
   }
 
 }
--- server/springboot-backend/springboot-backend/src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
+++ server/springboot-backend/springboot-backend/src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -2,9 +2,11 @@ package net.javaguides.springbootbackend.controller;
 
 import java.util.List;
 
+import net.javaguides.springbootbackend.exception.ResourceNotFoundException;
 import net.javaguides.springbootbackend.model.Employee;
 import net.javaguides.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -24,5 +26,26 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee =  employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee employee =  employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updateEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(updateEmployee);
     }
 }
