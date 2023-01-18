2bc81ada9466951b2f6a49e2947493ace9158747
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-01-18 18:06:19 -0300

    Angular - add Update Service / Component

--- client/angular-frontend/src/app/employee-list/employee-list.component.ts
+++ client/angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 
@@ -11,7 +12,8 @@ export class EmployeeListComponent implements OnInit {
 
   employees: Employee[];
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService,
+              private router: Router ) { }
 
   ngOnInit(): void {
     this.getEmployees();
@@ -22,5 +24,9 @@ export class EmployeeListComponent implements OnInit {
       this.employees = data;
     });
     console.log(this.employees);
+  }
+
+  updateEmployee(id: number){
+    this.router.navigate(['update-employee', id]);
   }
 }
